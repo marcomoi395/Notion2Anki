8ff07b350bbf0551b39add37e2d1c1691352bfee
youngmarco <marcomoi395@gmail.com>
2024-11-13 09:23:40 +0700

    fix: input data of formatting func

--- handlers/formatting.js
+++ handlers/formatting.js
@@ -38,7 +38,7 @@ const multipleChoice = (item, date) => {
             "Title": "What mean???",
             "Qtype (0=kprim,1=mc,2=sc)": "2",
             "Q_1": item.options[0],
-            "Q_2": item.options[2],
+            "Q_2": item.options[1],
             "Q_3": item.options[2],
             "Q_4": item.options[3],
             "Answers": "1 0 0 0",
